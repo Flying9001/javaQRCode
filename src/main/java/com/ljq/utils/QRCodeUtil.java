235a56d9c23b8a6def6b076a2bfb06eead0c7f74
lujunqiang <lujunqiang@ancun.com>
2018-01-17 17:37:32 +0800

    create javaQRCode project

---
+++ src/main/java/com/ljq/utils/QRCodeUtil.java
@@ -0,0 +1,24 @@
+package com.ljq.utils;
+
+import java.text.Format;
+
+/**
+ * @Desccription: 二维码读写工具类
+ * @author: Thaumaturge
+ * @email: lujunqiang@ancun.com
+ * @date: 2018/1/17
+ */
+public class QRCodeUtil {
+
+
+    public static boolean createQRCode(String content, String outPath, int weight, int hight, Format format){
+
+        String qrCodeFormat = "png";
+
+
+
+        return false;
+
+    }
+
+}
