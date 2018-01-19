86be57f52c48e92e6f1bd0b783ce36fa56ac6870
lujunqiang <lujunqiang@ancun.com>
2018-01-19 17:39:06 +0800

    update 修改二维码生成方法，准备添加生成带logo的二维码的功能

--- src/main/java/com/ljq/utils/QRCodeUtil.java
+++ src/main/java/com/ljq/utils/QRCodeUtil.java
@@ -2,7 +2,6 @@ package com.ljq.utils;
 
 import com.google.zxing.*;
 import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
-import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.common.HybridBinarizer;
 
@@ -10,8 +9,6 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Path;
-import java.text.Format;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -23,40 +20,95 @@ import java.util.Map;
  */
 public class QRCodeUtil {
 
+    // default size of QRCode
+    private static final int WIDTH_DEFUALT = 300;
+    private static final int HEIGHT_DEFUALT = 300;
+
+
     /**
      * generate QRCode image,String content length <= 500
      * @param content the content of the QRCode
      * @param qrCodePath the path of generated QRCode image without filename' extension
-     * @param format the extention of the generated QRCode image file
      * @param width the width of the generated QRCode image
      * @param height the height of the generated QRCode image
      *
      * @return boolean whether generating the QRCode image successfully
      * */
-    public static boolean createQRCode(String content, String qrCodePath, Format format,int width, int height ){
+    public static boolean createQRCode(String content, String qrCodePath, Map<EncodeHintType,Object> hintTypeMap,
+                                       int width, int height ){
 
-        // limit content length
+        // check the data
         if(content == null || content.equals("") || content.length() > 500){
             return false;
         }
-        // QRcode image format
-        String qrCodeFormat = "png";
-        Map<EncodeHintType,String> hintType  = new HashMap<>();
-        hintType.put(EncodeHintType.CHARACTER_SET,"utf-8");
-        try {
-            BitMatrix bitMatrix = new MultiFormatWriter().encode(content, BarcodeFormat.QR_CODE,width,height,hintType);
-            BufferedImage image = new BufferedImage(width,height,BufferedImage.TYPE_INT_RGB);
-            File qrCodeFile = new File(qrCodePath + "." + qrCodeFormat);
-            Path path = qrCodeFile.toPath();
-            MatrixToImageWriter.writeToPath(bitMatrix,qrCodeFormat,path);
+        if(width <= 0 || height <= 0){
+            width = WIDTH_DEFUALT;
+            height = HEIGHT_DEFUALT;
+        }
+        BufferedImage image = null;
 
-            return true;
+        try {
+            BitMatrix bitMatrix = new MultiFormatWriter().encode(content,BarcodeFormat.QR_CODE,width,height,hintTypeMap);
+
+
         } catch (WriterException e) {
             e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
         }
+
+
+        // QRcode image format
+//        String qrCodeFormat = "png";
+//        Map<EncodeHintType,String> hintType  = new HashMap<>();
+//        hintType.put(EncodeHintType.CHARACTER_SET,"utf-8");
+//        try {
+//            BitMatrix bitMatrix = new MultiFormatWriter().encode(content, BarcodeFormat.QR_CODE,width,height,hintType);
+//            BufferedImage image = new BufferedImage(width,height,BufferedImage.TYPE_INT_RGB);
+//            File qrCodeFile = new File(qrCodePath + "." + qrCodeFormat);
+//            Path path = qrCodeFile.toPath();
+//            MatrixToImageWriter.writeToPath(bitMatrix,qrCodeFormat,path);
+//
+//            return true;
+//        } catch (WriterException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
         return false;
+    }
+
+    /**
+     * generate QRCode image, and add logo picture to the QRCode,String content length <= 500 ,and String title <= 20
+     * @param content the content of the QRCode
+     * @param title title of the QRCode
+     * @param qrCodePath the path of generated QRCode image without filename' extension
+     * @param logoPath the logo picture,which is on the center of the QRCode image
+     * @param width the width of the generated QRCode image
+     * @param height the height of the generated QRCode image
+     *
+     * @return boolean
+     * */
+    public static boolean createQRCode(String content, String title, String qrCodePath, String logoPath, int width,
+           int height){
+        // check the data
+        if(content == null || content.equals("") || content.length()>500
+                || title.length() > 20){
+            return false;
+        }
+        if(width <= 0 || height <= 0){
+            width = WIDTH_DEFUALT;
+            height = HEIGHT_DEFUALT;
+        }
+
+
+
+
+
+
+
+
+
+        return false;
+
     }
 
     /**
